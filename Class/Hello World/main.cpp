0e8b32a86ba65521adcb11cfffac08f07cb700d9
kmai3 <kevin7mai@gmail.com>
2020-08-31 22:52:31 -0700

    Hello World Addition

---
+++ Class/Hello World/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: Kevin Mai
+ * Created on June 23, 2020, 8:13 AM
+ * Purpose: Hello World 
+ */
+
+//System Libraries
+#include <iostream> //I/O Library
+using namespace std;
+
+//User libraries 
+
+//Global Constants Only
+//Well Known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes 
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Display The Inputs/Outputs
+   cout<<"Hello World"<<endl; 
+    //Clean up the code, close files, deallocate memory, etc...
+    //Exit stage right
+    return 0;
+}
+
